84225a111060b966d899654924c4777132d71ce3
georgewood749 <georgewood749@gmail.com>
2022-09-23 11:49:58 +0100

    added some tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,2 +1,25 @@
 const request = require('supertest')
-const app = require('../')
+const app = require('../app.js')
+
+describe('API server', () => {
+    let api;
+
+    beforeAll(() => {
+        api = app.listen(5000, () => {
+            console.log(`Example app listening on port 5000`)
+        })
+    })
+
+    afterAll((done) => {
+        console.log('Stopping test server');
+        api.close(done)
+    })
+
+    it('responds to get / with status of 200', (done) => {
+        request(api).get('/').expect(200, done);
+    })
+
+    it('retrieves all cats', (done) => {
+        request(api).get('/cat').expect(200, done);
+    })
+})
