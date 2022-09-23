f51e3d5ed7e189c8cb30d0868f6fd78fddbf14a5
georgewood749 <georgewood749@gmail.com>
2022-09-23 11:42:53 +0100

    added cats array

---
+++ data.js
@@ -0,0 +1,21 @@
+const cats = [
+    {
+        id: 1,
+        name: 'Mittens',
+        image: 'assets/cat1.png'
+    },
+
+    {
+        id: 2,
+        name: 'Zelda',
+        image: 'assets/cat2.png'
+    },
+
+    {
+        id: 3,
+        name: 'Cecil',
+        image: 'assets/cat3.png'
+    }
+]
+
+module.exports = catsArray
